8caba7cccbcac665a6c9980219ee98b76162fc58
Dejan Pangercic <dejan.pangercic@cs.tum.edu>
2011-02-04 20:53:59 +0100

    fix due to pcl_ros::subscriber.h vinishing from pcl_ros

--- pcl_cloud_tools/src/pcl_hull_to_polygon.cpp
+++ pcl_cloud_tools/src/pcl_hull_to_polygon.cpp
@@ -1,19 +1,27 @@
-
+#include <ros/ros.h>
 #include <boost/bind.hpp>
 #include <boost/foreach.hpp>
-
+#include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl_ros/subscriber.h>
+#include <ros/subscriber.h>
 #include <geometry_msgs/PolygonStamped.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/ros/conversions.h>
 
-typedef pcl_ros::Subscriber<pcl::PointXYZ> PointCloudSubscriber;
+typedef pcl::PointXYZ Point;
+typedef pcl::PointCloud<Point> PointCloud;
 
-void pointCloudToPolygonCb(const PointCloudSubscriber::PointCloud::ConstPtr &cloud, ros::Publisher &polygon_pub)
+ros::Subscriber PointCloudSubscriber;
+ros::Publisher polygon_pub;
+
+void pointCloudToPolygonCb(const sensor_msgs::PointCloud2ConstPtr &input)
 {
+  PointCloud cloud;
+  pcl::fromROSMsg (*input, cloud);
   geometry_msgs::PolygonStamped polygon;
 
-  polygon.header = cloud->header;
-  BOOST_FOREACH(const pcl::PointXYZ &pt, cloud->points)
+  polygon.header = cloud.header;
+  BOOST_FOREACH(const pcl::PointXYZ &pt, cloud.points)
   {
     geometry_msgs::Point32 out_pt;
     out_pt.x = pt.x;
@@ -28,12 +36,9 @@ void pointCloudToPolygonCb(const PointCloudSubscriber::PointCloud::ConstPtr &clo
 int main(int argc, char *argv[])
 {
   ros::init(argc, argv, "pcl_hull_to_polygon");
-
   ros::NodeHandle nh("~");
-
-  ros::Publisher polygon_pub = nh.advertise<geometry_msgs::PolygonStamped>("output", 10);
-  PointCloudSubscriber pcl_subscription = PointCloudSubscriber(
-    nh, "input", 10, boost::bind(&pointCloudToPolygonCb, _1, polygon_pub));
+  polygon_pub = nh.advertise<geometry_msgs::PolygonStamped>("output", 10);
+  PointCloudSubscriber = nh.subscribe("input", 10, pointCloudToPolygonCb);
 
   ros::spin();
 }
