951677bad19263e587373a282cbea11442f86118
Dejan Pangercic <dejan.pangercic@gmail.com>
2011-04-01 18:41:34 +0200

    depth -> rgb

--- kinect_cleanup/src/object_grabber.cpp
+++ kinect_cleanup/src/object_grabber.cpp
@@ -162,18 +162,18 @@ class ObjectGrabber
       // If nothing is pointed at
       if (!to_select_)
         return;
-      std::cerr << "to_select_" << std::endl;
+      //      std::cerr << "to_select_" << std::endl;
       // If there is no TF data available
       ros::Time time = ros::Time::now();
       tf::TransformListener tf_listener;
-      bool found_transform = tf_listener.waitForTransform("right_hand", "openni_depth_optical_frame", time, ros::Duration(0.1));
+      bool found_transform = tf_listener.waitForTransform("right_hand", "openni_rgb_optical_frame", time, ros::Duration(1));
       if (!found_transform)
       {
         std::cerr << "no transform found" << std::endl;
         return;
       }
       tf::StampedTransform c2h_transform;
-      tf_listener.lookupTransform("right_hand", "openni_depth_optical_frame", time, c2h_transform);
+      tf_listener.lookupTransform("right_hand", "openni_rgb_optical_frame", time, c2h_transform);
       to_select_ = false;
 
       ROS_INFO_STREAM ("[" << getName ().c_str () << "] Received cloud: cloud time " << cloud_in->header.stamp);
@@ -228,7 +228,7 @@ class ObjectGrabber
       // Create a Convex Hull representation of the projected inliers
       chull_.setInputCloud (boost::make_shared<PointCloud> (cloud_projected));
       chull_.reconstruct (cloud_hull);
-      //ROS_INFO ("Convex hull has: %d data points.", (int)cloud_hull.points.size ());
+      ROS_INFO ("Convex hull has: %d data points.", (int)cloud_hull.points.size ());
       hull_pub_.publish (cloud_hull);
 
       // //Compute the area of the plane
